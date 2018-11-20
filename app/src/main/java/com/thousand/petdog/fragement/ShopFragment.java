a6bcc20c4c53d370acea56fae86daef331316da8
15093300823@163.com <15093300823@163.com>
2018-11-20 11:22:38 +0800

    注释掉商城

--- app/src/main/java/com/thousand/petdog/fragement/ShopFragment.java
+++ app/src/main/java/com/thousand/petdog/fragement/ShopFragment.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+/**
 import com.mob.shop.datatype.entity.Order;
 import com.mob.shop.gui.Callback;
 import com.mob.shop.gui.ShopGUI;
 import com.mob.shop.gui.pay.customizedpay.CustomizedPayListener;
+*/
 import com.thousand.petdog.R;
 
 
@@ -31,6 +32,7 @@ public class ShopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg_shop,container,false);
+        /**
         Callback callback = new Callback() {
             //跳转开发者应用的登录界面，等待用户进行登录操作，登录成功后调用MobSDK.setUser设置登录用户信息
             @Override
@@ -53,6 +55,8 @@ public class ShopFragment extends Fragment {
         };
         // 进入商城首页
         ShopGUI.showShopPage(callback);
+
+        */
         return view;
     }
 }
